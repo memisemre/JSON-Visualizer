7839fe2db2e46276fdbf393b6d9b0e69d487f012
memisemre <dev.emrememis@gmail.com>
2022-09-21 14:43:51 +0300

    Add Second Area

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const button = document.querySelector('.btn')
 button.addEventListener('click', function(){
-    const dataArea = document.querySelector('.data-area').value;
+    let dataArea = document.querySelector('.data-area').value;
+    console.log(typeof dataArea)
     let parseJSON = JSON.parse(dataArea);
-    for(let key in parseJSON){
+    // let evalDataArea = eval(parseJSON);
+    // console.log(evalDataArea);
+    // console.log(typeof parseJSON)
+    // console.log(typeof parseJSON)
+    // let mapJSON = parseJSON.map(map)
+    // console.log(mapJSON)
+    if (typeof parseJSON == 'object'){
+        for(let key in parseJSON){
+        // let mapJSON = parseJSON.map(key)
+        // console.log(mapJSON)
         if(parseJSON.hasOwnProperty(key)){
+            let parseJSONKey = parseJSON[key];
+            console.log(typeof parseJSONKey)
+            if (typeof parseJSONKey == 'object'){
+                for(let key in parseJSONKey){
+                    if(parseJSONKey.hasOwnProperty(key)){
+                        console.log(parseJSONKey[key])
+                        document.querySelector('.second-json-area').innerHTML += `${key} : ${parseJSONKey[key]} <br>`
+                    }
+                }
             document.querySelector('.json-area').innerHTML += `${key} : ${parseJSON[key]} <br>`
+            }
         }
+    }}
+    else {
+        console.log("hata")
     }
 })
