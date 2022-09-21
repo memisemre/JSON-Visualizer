083f0f67c18d755fac3b95bffd64acc512538470
memisemre <dev.emrememis@gmail.com>
2022-09-21 13:44:28 +0300

    Added For İn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const button = document.querySelector('.btn')
 button.addEventListener('click', function(){
     const dataArea = document.querySelector('.data-area').value;
     let parseJSON = JSON.parse(dataArea);
-    console.log(parseJSON)
-    parseJSON.forEach(dataPiece => {
-        document.querySelector('.json-area').innerHTML += `${dataPiece.name} <br>`
-    });
+    for(let key in parseJSON){
+        if(parseJSON.hasOwnProperty(key)){
+            document.querySelector('.json-area').innerHTML += `${key} : ${parseJSON[key]} <br>`
+        }
+    }
 })
